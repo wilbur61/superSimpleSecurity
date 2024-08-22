e58da4d6b0d8c713232eeb1a8e60a19afd6215a4
wilbur61 <wroales@perscholas.org>
2024-08-22 14:45:31 -0400

    Initial commit Darby Inspired Simple login

---
+++ src/main/java/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,47 @@
+package demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails wilbur = User.builder()
+                .username("wilbur")
+                .password("{noop}123")
+                .roles("SUPERUSER")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(wilbur, mary, susan);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
